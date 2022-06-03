7d105f17e1a7ce92a5aecfd9e29f52a48938ba63
NellySach <dldbstn147@naver.com>
2022-06-03 21:31:51 +0900

    note a little

--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class NoteManager : MonoBehaviour
 {
-    /* public int bpm = 0;
-     double currentTime = 0d;
+    public int bpm = 0;
+    double currentTime = 0d;
 
-     [SerializeField] Transform tfNoteAppear = null;
-     [SerializeField] GameObject goNote = null;
+    [SerializeField] Transform tfNoteAppear = null;
+    [SerializeField] GameObject goNote1 = null;
+    [SerializeField] GameObject goNote2 = null;
+    [SerializeField] GameObject goNote3 = null;
+    [SerializeField] GameObject goNote4 = null;
 
-     // Update is called once per frame
-     void Update()
-     {
-         currentTime += currentTime.deltaTime;
+    void Update()
+    {
+        int i = 2;
+        currentTime += Time.deltaTime;
 
-         if (currentTime >= 60d / bpm)
-         {
-             GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
-             currentTime -= 60d / bpm;
-         }
-     }*/
+        if (currentTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote4, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) 
+    {
+        if (collision.CompareTag("Note")) 
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
