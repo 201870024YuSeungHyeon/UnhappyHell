24828854392c7377b17032805a30264e76b7d732
NellySach <dldbstn147@naver.com>
2022-06-04 22:03:52 +0900

    Note

--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -6,22 +6,20 @@ public class NoteManager : MonoBehaviour
 {
     public int bpm = 0;
     double currentTime = 0d;
+    public GameObject[] NotePrefab;
 
     [SerializeField] Transform tfNoteAppear = null;
-    [SerializeField] GameObject goNote1 = null;
-    [SerializeField] GameObject goNote2 = null;
-    [SerializeField] GameObject goNote3 = null;
-    [SerializeField] GameObject goNote4 = null;
 
     void Update()
     {
-        int i = 2;
         currentTime += Time.deltaTime;
 
         if (currentTime >= 60d / bpm)
         {
-            GameObject t_note = Instantiate(goNote4, tfNoteAppear.position, Quaternion.identity);
+            int NoteIndex = Random.Range(0, NotePrefab.Length);
+            GameObject t_note = Instantiate(NotePrefab[NoteIndex], tfNoteAppear.position, Quaternion.identity);
             t_note.transform.SetParent(this.transform);
+           
             currentTime -= 60d / bpm;
         }
     }
