14c73c2b0685861e8e540bd6d232fbe73c7b7487
NellySach <dldbstn147@naver.com>
2022-06-05 16:29:53 +0900

    jebal

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public GameObject[] playerHp;
+    public GameObject canvas;
+
+    RectTransform hpBar;
+
+    public float height = 1.7f;
+
+    void Start()
+    {
+//        hpBar = Instantiate(hpImage, canvas.transform).GetComponent<RectTransform>();
+    }
+
+
+    void Update()
+    {
+   /*     Vector3 _hpBarPos =
+            Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
+        hpBar.position = _hpBarPos;*/
+    }
+}
