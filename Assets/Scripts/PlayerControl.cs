61985b51e210ff48426f00d46ac3000c1635eddb
NellySach <dldbstn147@naver.com>
2022-06-05 15:48:22 +0900

    change

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    double x;
+    [SerializeField] float jumpForce = 600f;
+    Vector2 startTouchPos, endTouchPos;
+    private Rigidbody2D rb;
+    bool jumpAllowed = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        TouchCheck();
+    }
+
+    void FixedUpdate()
+    {
+        JumpIfAllowed();
+
+
+    }
+
+    void TouchCheck()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPos = Input.GetTouch(0).position;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPos = Input.GetTouch(0).position;
+            if (endTouchPos.y > startTouchPos.y && rb.velocity.y == 0)
+            {
+                jumpAllowed = true;
+
+            }
+        }
+    }
+    void JumpIfAllowed()
+    {
+        if (jumpAllowed)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            jumpAllowed = false;
+        }
     }
 
     public void LButtonDown()
