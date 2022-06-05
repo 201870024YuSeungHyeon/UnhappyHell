44e0624a41bbbe8a544607bcf869a03e4d29a83b
NellySach <dldbstn147@naver.com>
2022-06-05 15:59:33 +0900

    reset

---
+++ Assets/Scripts/Note/Note1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note1 : MonoBehaviour
+{
+    public float noteSpeed = 400;
+    // Start is called before the first frame update
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localPosition += Vector3.down * noteSpeed * Time.deltaTime;
+        transform.localPosition += new Vector3(-12, -100, 0) * 3 * Time.deltaTime;
+    }
+}
