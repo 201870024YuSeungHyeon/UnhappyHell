f698699f58e60df37b90c48cca88c6f2c62bc68d
201870024YuSeungHyeon <jyyootom@naver.com>
2022-06-17 20:29:39 +0900

    흡수한 구슬 포지션 수정

--- Assets/Scripts/beadChange.cs
+++ Assets/Scripts/beadChange.cs
@@ -7,16 +7,19 @@ public class beadChange : MonoBehaviour
     AttackEvent attackEvent;
     public Sprite bead_Red, bead_blue, bead_Green, bead_Normal;
     SpriteRenderer thisImg;
+    PlayerControl playerControl;
     // Start is called before the first frame update
     void Start()
     {
         attackEvent = FindObjectOfType<AttackEvent>();
         thisImg = GetComponent<SpriteRenderer>();
+        playerControl = FindObjectOfType<PlayerControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.localPosition = playerControl.player_Pos + new Vector2(-608, -1016);
         if(attackEvent.bead == AttackEvent.beadB.normal)
         {
             thisImg.sprite = bead_Normal;
