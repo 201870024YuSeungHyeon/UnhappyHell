203f411dd2a7c4ada7cb02075b191f7bfd25aa1f
NellySach <dldbstn147@naver.com>
2022-06-18 01:04:29 +0900

    오류 해결

--- Assets/Scripts/PausePageControl.cs
+++ Assets/Scripts/PausePageControl.cs
@@ -24,7 +24,7 @@ public class PausePageControl : MonoBehaviour
 
     public void QuitButton()
     {
-
-        SceneManager.LoadScene("StageScene");
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainScene");
     }
 }
